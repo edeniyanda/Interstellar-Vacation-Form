73dd29c3d5443b0c2230966f577fe7d9372ddc05
Eden Iyanda <edeniyanda@gmail.com>
2023-04-27 03:59:52 +0100

    Refactored code for better readability and maintainability

--- app.py
+++ app.py
@@ -1,64 +1,75 @@
-# Import the Tkinter module and the messagebox module
+# import the necessary modules from tkinter
 from tkinter import *
+from tkinter.ttk import Combobox
 from tkinter import messagebox
 
-# Create the main application window
-app = Tk()
+# set the background color for the GUI
+bg_color = "#326273"
 
-# Set the window size and title
-app.geometry("500x300")
-app.title("Registration From")
+# create a new Tkinter window object
+root = Tk()
 
-# Create a label for the header of the form
-lblhead = Label(app, text="Interstellar Travel Form", font="arial 15 bold")
-lblhead.grid(row=0, column=3)
+# set the size, position, and title of the window
+root.geometry("700x400+300+200")
+root.title("Interstella Registry Form")
+root.resizable(False, False)
 
-# Define a function to display a message when the user submits the form
-def getval():
-    messagebox.showinfo("Status", "Data Stored")
+# set the background color of the window
+root.configure(bg=bg_color)
 
-# Create labels for each of the input fields on the form
-lbl_name = Label(app, text="Name:")
-lbl_age = Label(app, text="Age:")
-lbl_occupation = Label(app, text="Occupation:")
-lbl_phone_no = Label(app, text="Phone Number:")
-lbl_payment_method = Label(app, text="Payment Method:")
+# define a function to clear all the input fields
+def clear():
+    nameValue.set("")
+    contactValue.set('')
+    ageValue.set("")
+    addressEntry.delete(1.0, END)
 
-# Set the labels on the grid
-lbl_name.grid(row=1,column=2)
-lbl_age.grid(row=2,column=2)
-lbl_occupation.grid(row=3,column=2)
-lbl_phone_no.grid(row=4,column=2)
-lbl_payment_method.grid(row=5,column=2)
+# create a label for the heading
+Label(root, text="Please fill the form Below", font="arial 13", bg=bg_color).place(x=20, y=20)
 
-# Define StringVar and IntVar variables to store the user input for each field
-namevalue = StringVar
-agevalue = StringVar
-occupationvalue = StringVar
-phonenovalue = StringVar
-paymentmethodvalue = StringVar
-checkvalue = IntVar
+# create labels for each input field
+Label(root, text="Name", font="23", bg=bg_color, fg="#fff").place(x=50, y=100)
+Label(root, text="Contact No", font="23", bg=bg_color, fg="#fff").place(x=50, y=150)
+Label(root, text="Age", bg=bg_color, font="23", fg="#fff").place(x=50, y=200)
+Label(root, text="Gender", bg=bg_color, font="23", fg="#fff").place(x=370, y=200)
+Label(root, text="Address", bg=bg_color, fg="#fff", font="23").place(x=50, y=250)
 
-# Create Entry widgets for each field and link them to the corresponding variables
-name_entry = Entry(app, textvariable=namevalue)
-age_entry = Entry(app, textvariable= agevalue)
-occupation_entry = Entry(app, textvariable=occupationvalue)
-phone_no_entry = Entry(app, textvariable=phonenovalue)
-payment_method_entry = Entry(app, textvariable=paymentmethodvalue)
+# create input fields for each input field
+nameValue  = StringVar()
+contactValue = StringVar()
+ageValue = IntVar()
 
-# Set the Entry widgets on the grid
-name_entry.grid(row=1, column=3)
-age_entry.grid(row=2,column=3)
-occupation_entry.grid(row=3, column=3)
-phone_no_entry.grid(row=4, column=3)
-payment_method_entry.grid(row=5, column=3)
+# create an Entry widget for the name input field
+nameEntry = Entry(root, textvariable=nameValue, width=45, bd=2, font=20 )
+nameEntry.place(x=200, y=100)
 
-# Create a checkbox widget and link it to the checkvalue variable
-rdb_check = Checkbutton(text="Remind Me", variable=checkvalue)
-rdb_check.grid(row=6, column=3)
+# create an Entry widget for the contact input field
+contactEntry = Entry(root, textvariable=contactValue, width=45, bd=2, font=20 )
+contactEntry.place(x=200, y=150)
 
-# Create a Submit button which calls the getval() function when clicked
-Button(text="Submit", command=getval).grid(row=7, column=3)
+# create an Entry widget for the age input field
+ageEntry = Entry(root, textvariable=ageValue, width=15, bd=2, font=20 )
+ageEntry.place(x=200, y=200)
 
-# Start the main event loop to display the window and interact with the user
-app.mainloop()
+# create a Combobox widget for the gender input field
+gender = Combobox(root, values=["Male", "Female", "Prefer not to say"], font="arial 14",  state="r", width=14)
+gender.place(x=435, y= 200)
+gender.set("Male")
+
+# create a Text widget for the address input field
+addressEntry = Text(root, width=50, height=4, bd=4)
+addressEntry.place(x=200, y=250)
+
+# create buttons to submit the form, clear the fields, and exit the program
+btn_submit = Button(root, text="Submit", bg=bg_color,fg="white",width=15, height=2)
+btn_clear = Button(root, text="Clear", bg=bg_color,fg="white",width=15, height=2, command=clear)
+btn_exit = Button(root, text="Exit", bg=bg_color,fg="white",width=15, height=2, command= lambda: root.destroy())
+
+# position the buttons on the form
+btn_submit.place(x=200, y=345)
+btn_clear.place(x=340, y=345)
+btn_exit.place(x=480, y=345)
+
+
+# Run app
+root.mainloop()
